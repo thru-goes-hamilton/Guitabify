25b005bf8d274c0e531562101dee290f986ed393
thru-goes-hamilton <kamalu211211@gmail.com>
2024-10-19 22:33:15 +0530

    include port in flask

--- backend/app.py
+++ backend/app.py
@@ -60,4 +60,4 @@ def delete_file():
         return jsonify({'error': 'No file name provided'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
